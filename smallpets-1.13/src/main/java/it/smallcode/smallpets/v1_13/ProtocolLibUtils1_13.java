4f279ecf22cc9d77459d69428ea7bc1586bd3451
Hannes <42945799+hannest03@users.noreply.github.com>
2021-06-04 23:12:02 +0200

    Fixed bug with protocol on 1.13

--- smallpets-1.13/src/main/java/it/smallcode/smallpets/v1_13/ProtocolLibUtils1_13.java
+++ smallpets-1.13/src/main/java/it/smallcode/smallpets/v1_13/ProtocolLibUtils1_13.java
@@ -6,8 +6,36 @@ Class created by SmallCode
 
 */
 
-import it.smallcode.smallpets.v1_15.ProtocolLibUtils1_15;
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.events.PacketContainer;
+import com.comphenix.protocol.wrappers.WrappedChatComponent;
+import com.comphenix.protocol.wrappers.WrappedDataWatcher;
+import it.smallcode.smallpets.v1_12.ProtocolLibUtils1_12;
 
-public class ProtocolLibUtils1_13 extends ProtocolLibUtils1_15 {
+import java.util.Optional;
+
+public class ProtocolLibUtils1_13 extends ProtocolLibUtils1_12 {
+
+    @Override
+    public PacketContainer setCustomName(int entityID, String name) {
+
+        PacketContainer entityMetadata = ProtocolLibrary.getProtocolManager().createPacket(PacketType.Play.Server.ENTITY_METADATA);
+
+        entityMetadata.getIntegers().write(0, entityID);
+
+        WrappedDataWatcher dataWatcher = new WrappedDataWatcher(entityMetadata.getWatchableCollectionModifier().read(0));
+
+        dataWatcher.setObject(new WrappedDataWatcher.WrappedDataWatcherObject(2, WrappedDataWatcher.Registry.getChatComponentSerializer(true)), Optional
+                .of(WrappedChatComponent
+                        .fromChatMessage(name)[0].getHandle()));
+
+        dataWatcher.setObject(new WrappedDataWatcher.WrappedDataWatcherObject(3, WrappedDataWatcher.Registry.get(Boolean.class)), true);
+
+        entityMetadata.getWatchableCollectionModifier().write(0, dataWatcher.getWatchableObjects());
+
+        return entityMetadata;
+
+    }
 
 }
